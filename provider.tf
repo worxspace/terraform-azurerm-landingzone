5e73807d1db42c9cd2ae51d5cf1803a5827106f3
Ryan Bartram <ryan@bartram.ch>
2023-07-07 01:24:56 +0200

    add secondary provider alias for mi

--- provider.tf
+++ provider.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       version = ">=3.40.0"
+      configuration_aliases = [azurerm.mi]
     }
     azurecaf = {
       version = ">=2.0.0-preview3"
